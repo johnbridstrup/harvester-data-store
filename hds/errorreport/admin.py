a414e10563957152920404063186ff9c6c02e4b8
Kashif Aziz <kashif@diyainteractive.com>
2022-04-24 14:25:18 +0500

    add errorreport model, admin, fixtures

--- hds/errorreport/admin.py
+++ hds/errorreport/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import ErrorReport
 
-# Register your models here.
+
+class ErrorReportAdmin(admin.ModelAdmin):
+    list_display = ('reportTime', 'location', 'harvester', 'creator', 'modifiedBy')
+    ordering = ('location', 'harvester', 'reportTime', 'creator', 'modifiedBy')
+    search_fields = ('location__ranch', 'harvester__name', 'creator__username',)
+
+
+admin.site.register(ErrorReport, ErrorReportAdmin)
--- hds/errorreport/models.py
+++ hds/errorreport/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from common.models import CommonInfo
+from location.models import Location
+from harvester.models import Harvester
 
-# Create your models here.
+
+class ReportBase(CommonInfo):
+    """ ReportBase Model """
+    reportTime = models.DateTimeField(blank=True, null=True)
+    report = models.JSONField(blank=True, null=True)
+
+    class Meta:
+        abstract = True
+
+
+class ErrorReport(ReportBase):
+    """ ErrorReport Model """
+    location = models.ForeignKey(Location, on_delete=models.CASCADE, related_name='errorlocation')
+    harvester = models.ForeignKey(Harvester, on_delete=models.CASCADE, related_name='errorharvester')
+
