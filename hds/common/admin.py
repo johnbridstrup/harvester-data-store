9bc1684673dafdeceb13ec54d895603a5246f080
Kashif Aziz <kashif@diyainteractive.com>
2022-04-24 13:49:38 +0500

    fix search_fields in admin

--- hds/common/admin.py
+++ hds/common/admin.py
@@ -5,7 +5,7 @@ from .models import CommonInfo
 class CommonAdmin(admin.ModelAdmin):
     list_display = ('creator', 'modifiedBy', 'lastModified')
     ordering = ('creator', 'modifiedBy', 'lastModified')
-    search_fields = ('creator', 'modifiedBy')
+    search_fields = ('creator__username', 'modifiedBy__username')
 
 
 admin.site.register(CommonInfo, CommonAdmin)
