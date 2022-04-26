9bdf0326c86dd39661904cf76d5c518800cf9d48
Kashif Aziz <kashif@diyainteractive.com>
2022-04-26 15:23:55 +0500

    errorrreport model updated

--- hds/errorreport/models.py
+++ hds/errorreport/models.py
@@ -1,20 +1,10 @@
 from django.db import models
-from common.models import CommonInfo
+from common.models import ReportBase
 from location.models import Location
 from harvester.models import Harvester
-
-
-class ReportBase(CommonInfo):
-    """ ReportBase Model """
-    reportTime = models.DateTimeField(blank=True, null=True)
-    report = models.JSONField(blank=True, null=True)
-
-    class Meta:
-        abstract = True
 
 
 class ErrorReport(ReportBase):
     """ ErrorReport Model """
     location = models.ForeignKey(Location, on_delete=models.CASCADE, related_name='errorlocation')
     harvester = models.ForeignKey(Harvester, on_delete=models.CASCADE, related_name='errorharvester')
-
