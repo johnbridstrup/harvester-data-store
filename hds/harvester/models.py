b705ab1fc652703602d377e985f914ecef734b36
Kashif Aziz <kashif@diyainteractive.com>
2022-04-26 15:24:20 +0500

    fruit and harvester models updated

--- hds/harvester/models.py
+++ hds/harvester/models.py
@@ -1,18 +1,19 @@
 from django.db import models
 from location.models import Location
+from common.models import CommonInfo
 
 
-class Fruit(models.Model):
+class Fruit(CommonInfo):
     name = models.CharField(max_length=255)
 
     def __str__(self):
         return self.name
 
 
-class Harvester(models.Model):
+class Harvester(CommonInfo):
     harv_id = models.IntegerField()
     fruit = models.ForeignKey(Fruit, on_delete=models.CASCADE)
-    location = models.ForeignKey(Location, on_delete=models.CASCADE)  
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
     name = models.CharField(max_length=255, blank=True, null=True)
 
     def __str__(self):
