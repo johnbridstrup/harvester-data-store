98f0eeac1d0f0c4043621a9d771de91adc5163ea
Kashif Aziz <kashif@diyainteractive.com>
2022-04-26 15:23:35 +0500

    commoninfo model is abstract now, admin updated

--- hds/common/admin.py
+++ hds/common/admin.py
@@ -1,11 +1,2 @@
 from django.contrib import admin
-from .models import CommonInfo
 
-
-class CommonAdmin(admin.ModelAdmin):
-    list_display = ('creator', 'modifiedBy', 'lastModified')
-    ordering = ('creator', 'modifiedBy', 'lastModified')
-    search_fields = ('creator__username', 'modifiedBy__username')
-
-
-admin.site.register(CommonInfo, CommonAdmin)
--- hds/common/models.py
+++ hds/common/models.py
@@ -3,9 +3,23 @@ from django.contrib.auth.models import User
 
 
 class CommonInfo(models.Model):
-    creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name='creator')
-    modifiedBy = models.ForeignKey(User, on_delete=models.CASCADE, related_name='modifiedBy', blank=True, null=True)
+    creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name="%(app_label)s_%(class)s_creator_related")
+    modifiedBy = models.ForeignKey(User, on_delete=models.CASCADE, related_name="%(app_label)s_%(class)s_modifiedby_related",
+                                   blank=True, null=True)
+    created = models.DateTimeField(auto_now_add=True)
     lastModified = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return str(self.lastModified)
+
+    class Meta:
+        abstract = True
+
+
+class ReportBase(CommonInfo):
+    """ ReportBase Model """
+    reportTime = models.DateTimeField(blank=True, null=True)
+    report = models.JSONField(blank=True, null=True)
+
+    class Meta:
+        abstract = True
