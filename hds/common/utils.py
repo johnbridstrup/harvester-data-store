b0d796706c566c497bc84828ea0af9fbc89e10c5
Kashif Aziz <kashif@diyainteractive.com>
2022-05-15 20:08:50 +0500

    remove print statements

--- hds/common/utils.py
+++ hds/common/utils.py
@@ -1,11 +1,9 @@
 """ utilities and helper functions """
 from rest_framework.response import Response
-from rest_framework import status
 
 
 def sendresponse(response_status, response_message, response_data, status_code):
     """ send response to client """
-    print("status = ", response_status)
     if response_status == 'success':
         return Response({
             'status': response_status,
@@ -13,7 +11,6 @@ def sendresponse(response_status, response_message, response_data, status_code):
             'data': response_data
         }, status=status_code)
     else:
-        print(" -- here ")
         return Response({
             'status': response_status,
             'errors': response_message
