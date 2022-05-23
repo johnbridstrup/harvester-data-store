9f3f2ffeb0b58529dc9bb267c93da5999f503f9b
Kashif Aziz <kashif@diyainteractive.com>
2022-05-23 15:10:21 +0500

    update debug value based on environment variable

--- hds/hds/settings.py
+++ hds/hds/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-rzr+x&83_l1%9sc-hj)!7i8!^*^s&(+5v1v8vehxoyd5(8f_%x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', True)
 
 ALLOWED_HOSTS = []
 
