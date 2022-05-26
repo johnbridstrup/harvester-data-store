f755bc1faee7a43fa84ba2b0e3f0758326cc6573
John Bridstrup <john@advanced.farm>
2022-05-26 13:53:27 -0700

    healthcheck tests

--- hds/healthcheck/tests.py
+++ hds/healthcheck/tests.py
@@ -1,3 +1,21 @@
-from django.test import TestCase
+""" Test Fruits APIs """
+from rest_framework.test import APITestCase
+from rest_framework.test import APIClient
+from rest_framework.authtoken.models import Token
+from django.contrib.auth.models import User
 
-# Create your tests here.
+
+class HealthcheckAPITest(APITestCase):
+    """ Test Healthcheck APIs """
+    def setUp(self):
+        self.client = APIClient()
+        self.user = User.objects.create(username='test_user')
+        self.token = Token.objects.create(user=self.user)
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+        self.api_base_url = '/api/v1'
+
+    def test_healthcheck_basic(self):
+        response = self.client.get(f'{self.api_base_url}/healthcheck/')
+
+        assert response.status_code == 200
+        assert response.data['status'] == 'online'
