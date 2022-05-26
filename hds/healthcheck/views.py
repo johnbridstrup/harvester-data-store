661a57c74d18d8b20256103996b82f11e6a1c287
John Bridstrup <john@advanced.farm>
2022-05-26 13:40:08 -0700

    check debug mode

--- hds/healthcheck/views.py
+++ hds/healthcheck/views.py
@@ -2,6 +2,7 @@ from collections import OrderedDict
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from django.conf import settings
 
 from .checks.db import check as db_check
 from .checks.host import check as host_check
@@ -12,6 +13,7 @@ class HealthCheckView(APIView):
     def get(self, request, *args, **kwargs):
         response = OrderedDict()
         response['status'] = 'online'
+        response['debug'] = settings.DEBUG
 
         response['host'] = host_check(request)
         response['dbs'] = db_check(request)
