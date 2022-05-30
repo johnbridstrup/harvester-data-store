d6750b6109d172bd86ee77253aad20238bee4965
John Bridstrup <john@advanced.farm>
2022-05-30 10:12:51 -0700

    check for errors

--- hds/common/renderers.py
+++ hds/common/renderers.py
@@ -1,4 +1,9 @@
 from rest_framework.renderers import JSONRenderer
+import logging
+
+
+class NoContextError(Exception):
+    pass
 
 
 class HDSJSONRenderer(JSONRenderer):
@@ -10,24 +15,34 @@ class HDSJSONRenderer(JSONRenderer):
         'DELETE': 'deleted'
     }
 
+    SUCCESS = "success"
+
     def render(self, data, accepted_media_type=None, renderer_context=None):
         resp = self._create_response(data, renderer_context)
         return super().render(resp, accepted_media_type, renderer_context)
 
     def _create_response(self, data, context):
-        if context is not None:
+        if context is None:
+            logging.error("No context was provided to the renderer")
+            status = "error"
+            msg = "No context provided to the renderer"
+
+        else:
             model = context['view'].basename
             method = context['request']._request.method
+
+            if isinstance(data, dict):
+                status = data.get('status', HDSJSONRenderer.SUCCESS)
+            else:
+                status = HDSJSONRenderer.SUCCESS
+
+            if status != HDSJSONRenderer.SUCCESS:
+                return data
 
             msg = "{} {} successfully".format(
                 model, 
                 self.METHODS.get(method, f'Unexpected Op {method}')
             )
-            status = "success"
-        
-        else:
-            msg = "No context"
-            status = "unknown"
 
         response = {
             'status': status,
