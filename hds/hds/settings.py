6bd408a90a8cf650fd2894706366e08a479f9ec4
Kashif Aziz <kashif@diyainteractive.com>
2022-07-05 17:26:59 +0500

    fix static assets availability issue

--- hds/hds/settings.py
+++ hds/hds/settings.py
@@ -163,11 +163,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
- os.path.join(BASE_DIR, 'static')
+ os.path.join(BASE_DIR, 'hds/static')
  ]
 
-# STATIC_ROOT = os.environ.get('STATIC_ROOT', os.path.join(BASE_DIR, 'static/')) 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.environ.get('STATIC_ROOT', os.path.join(BASE_DIR, 'static/'))
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.environ.get('MEDIA_ROOT', os.path.join(BASE_DIR, 'media/'))
