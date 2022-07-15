8b3207d17ed0345a4939ee50fe22807757263581
John Bridstrup <john@advanced.farm>
2022-07-15 08:07:21 -0700

    Remove makemigrations from CI script
    These should be version controlled and tested.
    It was a mistake having this here initially

--- scripts/start-dev-server.sh
+++ scripts/start-dev-server.sh
@@ -4,7 +4,6 @@ cd $HDS_ROOT
 echo "" && echo "Build"
 docker compose up -d --build
 echo "" && echo "Migrate"
-docker compose exec web python hds/manage.py makemigrations && \
 docker compose exec web python hds/manage.py migrate
 echo "" && echo "Populate initial data"
 docker compose exec web python hds/manage.py loaddata fixtures/*
