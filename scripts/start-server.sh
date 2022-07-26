037881424ca93c332a945a47aae0334407564f55
John Bridstrup <john@advanced.farm>
2022-07-26 10:01:36 -0700

    initsuperuser on startup
    This is only necessary in the cloud. Local development uses fixtures for superusers

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -5,5 +5,7 @@ set -e
 cd hds
 echo "Migrating"
 python manage.py migrate
-echo "starting"
+echo "Creating Superuser"
+python manage.py initsuperuser
+echo "Starting"
 gunicorn hds.wsgi --user www-data --bind 0.0.0.0:$1 --workers 3
