54fd64605c987dadc420cab8d9a1de1e07e474de
John Bridstrup <john@advanced.farm>
2022-07-26 14:55:17 -0700

    PROMETHEUS_EXPORT MIGRATIONS flag

--- hds/hds/settings.py
+++ hds/hds/settings.py
@@ -184,3 +184,9 @@ CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
 
 PROMETHEUS_LATENCY_BUCKETS = (.1, .2, .5, .6, .8, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.5, 9.0, 12.0, 15.0, 20.0, 30.0, float("inf"))
+
+# Causes errors during testing if True
+PROMETHEUS_EXPORT_MIGRATIONS = os.environ.get(
+    'PROMETHEUS_EXPORT_MIGRATIONS',
+    'false'
+) in ['true', 'True', '1']
