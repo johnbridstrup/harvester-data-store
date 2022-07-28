e5bb5ace86484ad868929df73c1a8f03a6a21cf2
John Bridstrup <john@advanced.farm>
2022-07-28 10:57:12 -0700

    serialize code object in exceptions response

--- hds/exceptions/serializers.py
+++ hds/exceptions/serializers.py
@@ -1,3 +1,4 @@
+from django.http import QueryDict
 from rest_framework import serializers
 from .models import AFTExceptionCode, AFTException
 
@@ -9,11 +10,25 @@ class AFTExceptionCodeSerializer(serializers.ModelSerializer):
         read_only_fields = ('creator',)
 
 class AFTExceptionSerializer(serializers.ModelSerializer):
+    def to_representation(self, instance):
+        exceptions = super().to_representation(instance)
+
+        # Serialize AFTExceptionCode object
+        code = AFTExceptionCode.objects.get(id=exceptions['code'])
+        code_data = AFTExceptionCodeSerializer(code).data
+
+        exceptions['code'] = code_data
+        return exceptions
+    
     def to_internal_value(self, data):
         code = AFTExceptionCode.objects.get(code=data['code'])
-        data._mutable = True
-        data['code'] = code.pk
-        data._mutable = False
+        if isinstance(data, QueryDict):
+            # This is required for tests to pass
+            data._mutable = True
+            data['code'] = code.pk
+            data._mutable = False
+        else:
+            data['code'] = code.pk
         return super().to_internal_value(data)
 
     class Meta:
