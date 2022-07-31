98b657ac763c0bc0fd065b1b4bedd38d0115ae72
John Bridstrup <john@advanced.farm>
2022-07-31 15:53:45 -0700

    refactor error report api tests

--- hds/errorreport/tests/test_errorreport_api.py
+++ hds/errorreport/tests/test_errorreport_api.py
@@ -1,5 +1,5 @@
 """ Test ErrorReport APIs """
-from rest_framework.test import APITestCase
+from common.tests import HDSAPITestBase
 from rest_framework.test import APIClient
 from rest_framework.authtoken.models import Token
 from harvester.models import Harvester, Fruit
@@ -14,29 +14,18 @@ import json
 import os
 
 
-class ErrorReportAPITest(APITestCase):
+class ErrorReportAPITest(HDSAPITestBase):
     def setUp(self):
-        self.client = APIClient()
-        self.user = User.objects.create(username='test_user')
-        self.distributor = Distributor.objects.create(name='Distributor 1', creator=self.user)
-        self.location = Location.objects.create(
-            distributor=self.distributor, ranch='Ranch 1', country='USA', region='Region 1', creator=self.user)
-        self.fruit = Fruit.objects.create(name='Strawberry', creator=self.user)
-        self.harvester = Harvester.objects.create(harv_id=11, fruit=self.fruit, location=self.location, name='Harvester 1', creator=self.user)
-        self.code = AFTExceptionCode.objects.create(code=9, name='test', team='test', cycle=False, creator=self.user)
-        self.token = Token.objects.create(user=self.user)
-        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
-        self.api_base_url = '/api/v1'
+        super().setUp()
+        self.test_objects = self._setup_basic()
 
-        # initialize data
-        # open report.json
-        report_json_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'report.json')
+        report_json_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'report.json') 
         with open(report_json_path) as f:
             self.data = json.load(f)
 
     def test_create_errorreport(self):
         """ create error report and assert it exists """
-        self.client.post(f'{self.api_base_url}/errorreports/', self.data, format='json', HTTP_ACCEPT='application/json')
+        r= self.client.post(f'{self.api_base_url}/errorreports/', self.data, format='json', HTTP_ACCEPT='application/json')
         self.assertEqual(ErrorReport.objects.count(), 1)
         self.assertEqual(AFTException.objects.count(), 1)
 
@@ -54,8 +43,8 @@ class ErrorReportAPITest(APITestCase):
         report_time = make_aware(datetime.datetime.fromtimestamp(data["timestamp"]))
 
         ErrorReport.objects.create(
-            creator=self.user, location=self.location,
-            harvester=self.harvester, reportTime=report_time, report=data)
+            creator=self.user, location=self.test_objects['location'],
+            harvester=self.test_objects["harvester"], reportTime=report_time, report=data)
 
         # updating reportTime
         current_time = make_aware(datetime.datetime.now().replace(microsecond=0))
@@ -71,8 +60,8 @@ class ErrorReportAPITest(APITestCase):
         report_time = make_aware(datetime.datetime.fromtimestamp(data["timestamp"]))
 
         ErrorReport.objects.create(
-            creator=self.user, location=self.location,
-            harvester=self.harvester, reportTime=report_time, report=data)
+            creator=self.user, location=self.test_objects["location"],
+            harvester=self.test_objects["harvester"], reportTime=report_time, report=data)
 
         # updating harv_id
         response = self.client.patch(
@@ -86,8 +75,8 @@ class ErrorReportAPITest(APITestCase):
         report_time = make_aware(datetime.datetime.fromtimestamp(data["timestamp"]))
 
         ErrorReport.objects.create(
-            creator=self.user, location=self.location,
-            harvester=self.harvester, reportTime=report_time, report=data)
+            creator=self.user, location=self.test_objects["location"],
+            harvester=self.test_objects["harvester"], reportTime=report_time, report=data)
 
         self.client.delete(f'{self.api_base_url}/errorreports/1/', HTTP_ACCEPT='application/json')
         self.assertEqual(ErrorReport.objects.count(), 0)
@@ -98,15 +87,15 @@ class ErrorReportAPITest(APITestCase):
         report_time = make_aware(datetime.datetime.fromtimestamp(data["timestamp"]))
 
         ErrorReport.objects.create(
-            creator=self.user, location=self.location,
-            harvester=self.harvester, reportTime=report_time, report=data)
+            creator=self.user, location=self.test_objects["location"],
+            harvester=self.test_objects["harvester"], reportTime=report_time, report=data)
 
         current_time = make_aware(datetime.datetime.now().replace(microsecond=0))
         new_timestamp = current_time.timestamp()
         data["timestamp"] = new_timestamp
         ErrorReport.objects.create(
-            creator=self.user, location=self.location,
-            harvester=self.harvester, reportTime=report_time, report=data)
+            creator=self.user, location=self.test_objects["location"],
+            harvester=self.test_objects["harvester"], reportTime=report_time, report=data)
 
         response = self.client.get(f'{self.api_base_url}/errorreports/', HTTP_ACCEPT='application/json')
         self.assertEqual(response.status_code, 200)
@@ -118,8 +107,8 @@ class ErrorReportAPITest(APITestCase):
         report_time = make_aware(datetime.datetime.fromtimestamp(data["timestamp"]))
 
         ErrorReport.objects.create(
-            creator=self.user, location=self.location,
-            harvester=self.harvester, reportTime=report_time, report=data)
+            creator=self.user, location=self.test_objects["location"],
+            harvester=self.test_objects["harvester"], reportTime=report_time, report=data)
 
         response = self.client.get(f'{self.api_base_url}/errorreports/1/')
         self.assertEqual(response.status_code, 200)
@@ -142,7 +131,7 @@ class ErrorReportAPITest(APITestCase):
     def test_generate_pareto(self):
         pareto_groups = ["code__code", "code__name", "service", "report__harvester__harv_id"]
         pareto_names = ["code", "exception", "service", "harvester"]
-        pareto_name_vals = ["9", "test", "traychg", "11"]
+        pareto_name_vals = ["0", "AFTBaseException", "traychg", "11"]
         num = 5
         for _ in range(num):
             self.client.post(f'{self.api_base_url}/errorreports/', self.data, format='json', HTTP_ACCEPT='application/json')
