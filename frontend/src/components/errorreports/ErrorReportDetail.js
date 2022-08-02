16dbc71c99aaabdd41f1077f9f08d7ef88c2cd07
kkthecompguy <hnkhosamomollo3@gmail.com>
2022-08-02 16:56:14 +0300

    display report json data

--- frontend/src/components/errorreports/ErrorReportDetail.js
+++ frontend/src/components/errorreports/ErrorReportDetail.js
@@ -1,8 +1,19 @@
+import { useSelector } from 'react-redux';
+import { Container } from "../styled";
 
 
 function ErrorReportDetail(props) {
+  const { report } = useSelector(state => state.errorreport);
+  let reportData;
+  if (report.report) {
+    reportData = JSON.stringify(report.report, undefined, 2)
+  }
   return (
-    <div>ErrorReportDetail</div>
+    <Container>
+      <div className="d-flex justify-content-center align-items-center">
+        <textarea style={{width: '100%', height: '400px'}} defaultValue={reportData}></textarea>
+      </div>
+    </Container>
   )
 }
 
