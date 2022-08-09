581cf3400f1da6275ab5a36d0346026cfa66dbac
John Bridstrup <john@advanced.farm>
2022-08-09 10:51:42 -0700

    add node env to build variables

--- frontend/make/hdsfrontend.mk
+++ frontend/make/hdsfrontend.mk
@@ -9,7 +9,10 @@ login:
 	aws ecr get-login-password --region $(AWS_REGION) --profile $(AWS_PROFILE) | docker login --username AWS --password-stdin $(AWS_ACCOUNT_ID).dkr.ecr.$(AWS_REGION).amazonaws.com
 
 build:
-	docker build --build-arg HDS_URL=$(HDS_URL) --build-arg FRONTEND_URL=$(FRONTEND_URL) -t $(AWS_ACCOUNT_ID).dkr.ecr.$(AWS_REGION).amazonaws.com/hds:$(NAME)-$(SHA) ../..
+	docker build --build-arg HDS_URL=$(HDS_URL) \
+	--build-arg FRONTEND_URL=$(FRONTEND_URL) \
+	--build-arg NODE_ENV=$(NODE_ENV) \
+	-t $(AWS_ACCOUNT_ID).dkr.ecr.$(AWS_REGION).amazonaws.com/hds:$(NAME)-$(SHA) ../..
 
 push:
 	docker push $(AWS_ACCOUNT_ID).dkr.ecr.$(AWS_REGION).amazonaws.com/hds:$(NAME)-$(SHA)
