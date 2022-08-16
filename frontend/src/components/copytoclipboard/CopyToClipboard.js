edd008a4e2100fdbfa0a07d9892c5942ee6ceaeb
kkthecompguy <hnkhosamomollo3@gmail.com>
2022-08-16 15:52:28 +0300

    copy default if no query url

--- frontend/src/components/copytoclipboard/CopyToClipboard.js
+++ frontend/src/components/copytoclipboard/CopyToClipboard.js
@@ -1,24 +1,28 @@
 import { useState } from "react";
 import { useSelector } from "react-redux";
-import { toast } from "react-toastify";
 import { ClipboardDiv } from "../styled";
 
 function CopyToClipboard(props) {
   const [copied, setCopied] = useState(false);
   const { queryUrl } = useSelector((state) => state.errorreport);
+  const copy = async (text) => {
+    try {
+      await navigator.clipboard.writeText(text);
+      setCopied(true);
+    } catch (error) {
+      console.log("error", error);
+    }
+    setTimeout(function () {
+      setCopied(false);
+    }, 3000);
+  };
   const copyURL = async () => {
     if (typeof queryUrl === "string" && queryUrl.length > 0) {
-      try {
-        await navigator.clipboard.writeText(queryUrl);
-        setCopied(true);
-      } catch (error) {
-        console.log("error", error);
-      }
-      setTimeout(function () {
-        setCopied(false);
-      }, 3000);
+      await copy(queryUrl);
     } else {
-      toast.error("could not copy the query url");
+      let public_url =
+        process.env.REACT_APP_HOSTED_URL || "http://localhost:3000";
+      await copy(public_url + "/errorreports");
     }
   };
   return (
