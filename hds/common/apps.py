feec2ee262365e6c392dc4d6bdd72a322b07c408
John Bridstrup <john@advanced.farm>
2022-08-17 11:27:11 -0700

    Multi-process prometheus client

--- hds/common/apps.py
+++ hds/common/apps.py
@@ -4,3 +4,13 @@ from django.apps import AppConfig
 class CommonConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'common'
+
+    def ready(self):
+        import os
+        if os.environ.get("PROMETHEUS_MULTIPROC_DIR", None):
+            from prometheus_client import values
+
+            values.ValueClass = values.MultiProcessValue(
+                process_identifier=os.getpid
+            )
+        
--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -3,11 +3,24 @@
 
 set -e
 cd hds
+
 echo "Migrating"
 python manage.py migrate
+
 echo "Creating Superuser"
 python manage.py initsuperuser
+
 echo "Creating SQS Token"
 python manage.py create_sqs_user
+
+echo "Creating prometheus multi-process directory"
+if [ -d "/opt/app/multiproc-tmp" ]
+then
+    rm -rf /opt/app/multiproc-tmp
+fi
+mkdir /opt/app/multiproc-tmp
+chown -R www-data:www-data /opt/app/multiproc-tmp
+export PROMETHEUS_MULTIPROC_DIR=/opt/app/multiproc-tmp
+
 echo "Starting"
-gunicorn hds.wsgi --user www-data --bind 0.0.0.0:$1 --workers 3
+gunicorn -c /opt/app/hds/gunicorn_conf.py hds.wsgi --user www-data --bind 0.0.0.0:$1 --workers 3
