b4a84c6b4a529b1d99032a2069bd09f39c59fbb8
John Bridstrup <john@advanced.farm>
2022-08-30 14:10:57 -0700

    [Deploy] production container images

--- terraform/prod/hds/hds.tf
+++ terraform/prod/hds/hds.tf
@@ -5,7 +5,7 @@ locals {
   file_queue_name          = "hds-file-queue"
   service_port             = "8000"
   service_name             = "hds"
-  service_docker_image     = "838860823423.dkr.ecr.us-west-1.amazonaws.com/hds:hds-staging-c89b13a"
+  service_docker_image     = "838860823423.dkr.ecr.us-west-1.amazonaws.com/hds:hds-staging-aed1288"
   healthcheck_path         = "/api/v1/healthcheck/"
   hds_superuser_pwd_id     = "hds_superuser_pwd"
   errorreport_queue_name   = "errorreport-queue"
