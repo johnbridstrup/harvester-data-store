fa9579e2066e88acdfc71a43b07ded60f262fb17
kkthecompguy <hnkhosamomollo3@gmail.com>
2022-08-30 20:30:41 +0300

    add put & patch methods to base service

--- frontend/src/features/base/service.js
+++ frontend/src/features/base/service.js
@@ -25,6 +25,24 @@ export const axiosService = {
     const res = await axios.get(url, this.config);
     return res.data.data;
   },
+  put: async function (url, token, data = {}) {
+    let csrftoken = localStorage.getItem("csrftoken");
+    if (typeof token === "string" && token.length > 0) {
+      this.config["headers"]["Authorization"] = `Token ${token}`;
+    }
+    this.config["headers"]["X-CSRFToken"] = csrftoken;
+    const res = await axios.put(url, data, this.config);
+    return res.data;
+  },
+  patch: async function (url, token, data = {}) {
+    let csrftoken = localStorage.getItem("csrftoken");
+    if (typeof token === "string" && token.length > 0) {
+      this.config["headers"]["Authorization"] = `Token ${token}`;
+    }
+    this.config["headers"]["X-CSRFToken"] = csrftoken;
+    const res = await axios.patch(url, data, this.config);
+    return res.data;
+  },
 };
 
 export const persistCSRFToken = async () => {
