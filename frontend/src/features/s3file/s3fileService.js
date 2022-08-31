96b5e87c9468cc6ac844caebe6cb8325db8466ee
John Bridstrup <john@advanced.farm>
2022-08-31 16:40:58 -0700

    [bugfix] temp fix download link

--- frontend/src/features/s3file/s3fileService.js
+++ frontend/src/features/s3file/s3fileService.js
@@ -1,9 +1,10 @@
 import { API_URL } from "../base/constants";
 import { axiosService } from "../base/service";
 
-const s3FileDownload = (endpoint, token) => {
+const s3FileDownload = async (endpoint, token) => {
   const s3fileUrl = `${API_URL}${endpoint}`;
-  return axiosService.download(s3fileUrl, token);
+  const resp = await axiosService.get(s3fileUrl, token);
+  return axiosService.download(resp.data, token);
 };
 
 const s3FileService = {
