fd4e5d8437f4889ae31fbe3fb1b062dc7bf3b2af
kkthecompguy <hnkhosamomollo3@gmail.com>
2022-09-15 19:03:31 +0300

    updated the slack id check

--- frontend/src/components/users/UsersList.js
+++ frontend/src/components/users/UsersList.js
@@ -84,7 +84,7 @@ function UsersList(props) {
               {users.map((user, index) => (
                 <tr key={index}>
                   <td>{user.username}</td>
-                  <td>{user.profile.slack_id}</td>
+                  <td>{user.profile?.slack_id}</td>
                   <td>{user.first_name}</td>
                   <td>{user.last_name}</td>
                   <td>{user.is_active ? "Active" : "Inactive"}</td>
