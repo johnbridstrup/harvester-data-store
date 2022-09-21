a1426db153f62a0e58e2cbb1278617991deb3ddb
kkthecompguy <hnkhosamomollo3@gmail.com>
2022-09-21 21:01:02 +0300

    added distributor modal

--- frontend/src/components/distributor/ListDistributor.js
+++ frontend/src/components/distributor/ListDistributor.js
@@ -1,22 +1,100 @@
-import { useSelector } from "react-redux";
+import { useRef, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { toast } from "react-toastify";
+import { SUCCESS } from "../../features/base/constants";
+import {
+  createDistributor,
+  listDistributors,
+  updateDistributor,
+} from "../../features/distributor/distributorSlice";
 import { Loader } from "../../utils/utils";
+import DistributorModal from "../modals/DistributorModal";
 import { LoaderDiv } from "../styled";
 import DistributorTable from "../tables/DistributorTable";
 
 function ListDistributor(props) {
+  const [fieldData, setFieldData] = useState({
+    name: "",
+    mode: "add",
+    objId: null,
+  });
   const { distributors, loading } = useSelector((state) => state.distributor);
+  const distributorRef = useRef();
+  const dispatch = useDispatch();
+
+  const addPopUp = (mode) => {
+    if (typeof mode === "string" && mode === "add") {
+      setFieldData((current) => {
+        return { ...current, name: "", mode: "add", objId: null };
+      });
+    }
+    distributorRef.current.click();
+  };
+
+  const handleFieldChange = (e) => {
+    setFieldData((current) => {
+      return { ...current, [e.target.name]: e.target.value };
+    });
+  };
+  const handleFormSubmit = async (e) => {
+    e.preventDefault();
+    let data = {
+      name: fieldData.name,
+      objId: fieldData.objId,
+    };
+    const dispatchObj = {
+      add: createDistributor,
+      edit: updateDistributor,
+    };
+
+    const res = await dispatch(dispatchObj[fieldData.mode](data));
+    if (res.payload?.status === SUCCESS) {
+      await dispatch(listDistributors());
+      toast.success(res?.payload?.message);
+      addPopUp();
+      setFieldData((current) => {
+        return { ...current, name: "", mode: "add", objId: "" };
+      });
+    }
+  };
+
+  const handleDistUpdateClick = (obj) => {
+    setFieldData((current) => {
+      return { ...current, name: obj.name, mode: "edit", objId: obj.id };
+    });
+    addPopUp();
+  };
+
   return (
     <>
       <div className="flex-right">
-        <button className="btn btn-primary">Add New Distributor</button>
+        <button onClick={() => addPopUp("add")} className="btn btn-primary">
+          Add New Distributor
+        </button>
+        <button
+          ref={distributorRef}
+          data-bs-toggle="modal"
+          data-bs-target="#distributorModal"
+          style={{ display: "none" }}
+        >
+          Add New Distributor
+        </button>
       </div>
       {loading ? (
         <LoaderDiv>
           <Loader size={50} />
         </LoaderDiv>
       ) : (
-        <DistributorTable distributors={distributors} />
+        <DistributorTable
+          distributors={distributors}
+          handleDistUpdateClick={handleDistUpdateClick}
+        />
       )}
+      <DistributorModal
+        fieldData={fieldData}
+        handleChange={handleFieldChange}
+        handleSubmit={handleFormSubmit}
+      />
     </>
   );
 }
---
+++ frontend/src/components/modals/DistributorModal.js
@@ -0,0 +1,79 @@
+import PropTypes from "prop-types";
+import { Loader } from "../../utils/utils";
+
+function DistributorModal(props) {
+  const { name, mode } = props.fieldData;
+  return (
+    <div className="col-md-8">
+      <div
+        className="modal fade"
+        id="distributorModal"
+        tabIndex={-1}
+        role="dialog"
+        aria-labelledby="exampleModalCenterTitle"
+        aria-hidden="true"
+        style={{ display: "none" }}
+      >
+        <div className="modal-dialog modal-dialog-centered" role="document">
+          <div className="modal-content profile-modal">
+            <div className="text-right">
+              <button
+                type="button"
+                className="btn closeModalBtn"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              >
+                <span className="las la-times"></span>
+              </button>
+            </div>
+            <div className="modal-body text-center px-5 pb-2">
+              {mode === "edit" ? "UPDATE" : "ADD NEW"} DISTRIBUTOR
+            </div>
+
+            <div className="modal-body px-5 pb-4">
+              <form onSubmit={props.handleSubmit}>
+                <div className="row">
+                  <div className="col">
+                    <div className="form-group">
+                      <label htmlFor="name">Name</label>
+                      <input
+                        type="text"
+                        className="form-control"
+                        name="name"
+                        value={name}
+                        required
+                        onChange={props.handleChange}
+                      />
+                    </div>
+                  </div>
+                </div>
+                <div className="text-center">
+                  <button
+                    type="submit"
+                    className="btn btn-block btn-primary mt-4 mb-4"
+                  >
+                    {props.loading ? (
+                      <Loader size={25} />
+                    ) : mode === "edit" ? (
+                      "EDIT"
+                    ) : (
+                      "ADD"
+                    )}
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+DistributorModal.propTypes = {
+  fieldData: PropTypes.object,
+  handleChange: PropTypes.func,
+  handleSubmit: PropTypes.func,
+};
+
+export default DistributorModal;
