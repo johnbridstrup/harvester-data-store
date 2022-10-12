afac674c863bc8267467c97d19a09594012cdcc2
kkthecompguy <hnkhosamomollo3@gmail.com>
2022-10-12 19:46:25 +0300

    filter notification api

--- hds/notifications/views.py
+++ hds/notifications/views.py
@@ -10,3 +10,20 @@ class NotificationView(CreateModelViewSet):
     serializer_class = NotificationSerializer
     permission_classes = (IsAuthenticated,)
     http_method_names = ['get', 'delete']
+
+    def get_queryset(self):
+        filter_dict = {}
+
+        # get category from request
+        query_param = self.request.query_params.get('category')
+
+        # filter queryset for created
+        if query_param == "created":
+            filter_dict['creator'] = self.request.user
+
+        # filter queryset for is_recipient
+        if query_param == "is_recipient":
+            filter_dict["recipients__in"] = [self.request.user]
+
+        # return queryset
+        return self.queryset.filter(**filter_dict).order_by('-id').distinct()
