180d72c95a30e5f56348b7c34facc0f616471ce6
John Bridstrup <john@advanced.farm>
2022-10-20 16:19:12 -0700

    GET event tags endpoint
    This will be for listing available event tags if we add something like a dropdown for selecting tags

--- hds/event/tests.py
+++ hds/event/tests.py
@@ -1,17 +1,19 @@
 from common.tests import HDSAPITestBase
-from errorreport.models import ErrorReport
 from event.models import Event
+from errorreport.models import ErrorReport
+from s3file.models import S3File
 
+from rest_framework import status
 from django.urls import reverse
-
-
 class EventApiTestCase(HDSAPITestBase):
     def setUp(self):
         super().setUp()
         self._setup_basic()
         self.update_user_permissions_all(ErrorReport)
         self.update_user_permissions_all(Event)
+        self.update_user_permissions_all(S3File)
         self.event_url = reverse("event-list")
+        self.tags_endpoint = f"{self.event_url}tags/"
 
     def test_get_by_UUID(self):
         self._post_error_report()
@@ -26,4 +28,18 @@ class EventApiTestCase(HDSAPITestBase):
         # 1 event with requested UUID
         resp = self.client.get(self.event_url + f"?UUID={UUID}")
         self.assertEqual(resp.json()["data"]["count"], 1)
+
+    def test_event_tags(self):
+        self._setup_s3file()
+        r=self._post_error_report()
+        r=self.create_s3file()
+
+        r = self.client.get(self.tags_endpoint)
+        self.assertEqual(r.status_code, status.HTTP_200_OK)
+        
+        data = r.json()["data"]
+        expect = [self.filetype, ErrorReport.__name__]
+
+        for tag in expect:
+            self.assertIn(tag, data["tags"])
         
--- hds/event/views.py
+++ hds/event/views.py
@@ -1,6 +1,9 @@
-from .models import Event
+from .models import Event, EventTag
 from .serializers import EventSerializer
 
+from rest_framework.decorators import action
+from rest_framework.renderers import JSONRenderer
+from common.utils import make_ok
 from common.viewsets import CreateModelViewSet
 
 
@@ -9,3 +12,20 @@ class EventView(CreateModelViewSet):
     serializer_class = EventSerializer
     filterset_fields = ('UUID',)
     ordering = ('-id',)
+
+    def get_queryset(self):
+        filter_dict = {}
+        if "UUID" in self.request.query_params:
+            filter_dict['UUID'] = self.request.query_params.get("UUID")
+        return self.queryset.filter(**filter_dict).order_by('-id').distinct()
+        
+    @action(
+        methods=["get"],
+        detail=False,
+        url_path="tags",
+        renderer_classes=[JSONRenderer]
+    )
+    def get_tags(self, request):
+        queryset = Event.tags.all().values_list("name")
+        tags = [tag[0] for tag in queryset]
+        return make_ok("Event tags", {"tags": tags})
