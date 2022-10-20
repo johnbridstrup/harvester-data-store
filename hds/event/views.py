df17a2750dc71e3a107df0f3e2b156362896c1dc
kkthecompguy <hnkhosamomollo3@gmail.com>
2022-10-20 17:01:11 +0300

    added view filter

--- hds/event/views.py
+++ hds/event/views.py
@@ -9,3 +9,9 @@ class EventView(CreateModelViewSet):
     queryset = Event.objects.all()
     serializer_class = EventSerializer
     permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        filter_dict = {}
+        if "UUID" in self.request.query_params:
+            filter_dict['UUID'] = self.request.query_params.get("UUID")
+        return self.queryset.filter(**filter_dict).order_by('-id').distinct()
