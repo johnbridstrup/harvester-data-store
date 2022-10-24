223b3c468957cdbac0b5002a61e741e162ce806e
John Bridstrup <john@advanced.farm>
2022-10-24 08:41:04 -0700

    [testing] test event UUID param

--- hds/event/tests.py
+++ hds/event/tests.py
@@ -1,3 +1,29 @@
-from django.test import TestCase
+from common.tests import HDSAPITestBase
+from errorreport.models import ErrorReport
+from event.models import Event
 
-# Create your tests here.
+from django.urls import reverse
+
+
+class EventApiTestCase(HDSAPITestBase):
+    def setUp(self):
+        super().setUp()
+        self._setup_basic()
+        self.update_user_permissions_all(ErrorReport)
+        self.update_user_permissions_all(Event)
+        self.event_url = reverse("event-list")
+
+    def test_get_by_UUID(self):
+        self._post_error_report()
+        self._post_error_report()
+        data = self._post_error_report()
+        UUID = data["data"]["event"]["UUID"]
+
+        # 3 total events
+        all_resp = self.client.get(self.event_url)
+        self.assertEqual(all_resp.json()["data"]["count"], 3)
+
+        # 1 event with requested UUID
+        resp = self.client.get(self.event_url + f"?UUID={UUID}")
+        self.assertEqual(resp.json()["data"]["count"], 1)
+        
--- hds/event/urls.py
+++ hds/event/urls.py
@@ -3,7 +3,7 @@ from .views import EventView
 
 
 router = routers.SimpleRouter()
-router.register(r'', EventView)
+router.register(r'', EventView, basename="event")
 
 urlpatterns = [
 ] + router.urls
