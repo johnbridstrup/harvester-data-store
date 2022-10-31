84e522276be25511fedb8e6869bee3d6150d7566
John Bridstrup <john@advanced.farm>
2022-10-31 14:50:14 -0700

    [bugfix] SlackApiError can't be serialized
    This was causing a bug where if, for instance, a channel couldn't be found we'd get an unhandled exception
    and the task results wouldn't be complete. For now I'm just creating a new exception and storing the error
    key in it, but this is probably a good case for more robust error info handling as we had discussed a while
    back.

--- hds/notifications/slack.py
+++ hds/notifications/slack.py
@@ -2,18 +2,27 @@ from slack_sdk import WebClient
 from slack_sdk.errors import SlackApiError
 from common.async_metrics import ASYNC_ERROR_COUNTER
 import os
+from collections.abc import Mapping
 
 
 TOKEN = os.environ.get("SLACK_TOKEN")
+
+class SlackError(Exception):
+    # Unfortunately SlackApiErrors can't be serialized to JSON, to celery complains heavily
+    # if an exception is ever raised (channel not found, for instance).
+    pass
 
 def post_to_slack(message, channel='hds-test'):
     if TOKEN:
         client = WebClient(token=TOKEN)
         try:
-            client.chat_postMessage(text=message, channel=channel)
+            if isinstance(message, Mapping):
+                client.chat_postMessage(**message)
+            else:
+                client.chat_postMessage(text=message, channel=channel)
         except SlackApiError as e:
             ASYNC_ERROR_COUNTER.labels("post_to_slack", "SlackApiError", e.response['error']).inc()
-            raise e
+            raise SlackError(e.response['error'])
         return "Posted to slack"
     
     ASYNC_ERROR_COUNTER.labels("post_to_slack", "none", "no_token").inc()
