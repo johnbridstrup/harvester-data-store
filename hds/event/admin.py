b6cfa0eba227c6b14d4e683b7c773a964e4284f6
John Bridstrup <john@advanced.farm>
2022-11-10 08:41:30 -0800

    Add tag list to admin panel

--- hds/event/admin.py
+++ hds/event/admin.py
@@ -3,9 +3,15 @@ from .models import Event
 
 
 class EventAdmin(admin.ModelAdmin):
-    list_display = ('created', 'UUID')
+    list_display = ('created', 'UUID', 'tag_list')
     ordering = ('created',)
     search_fields = ('created', 'UUID')
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).prefetch_related('tags')
+
+    def tag_list(self, obj):
+        return u", ".join(o.name for o in obj.tags.all())
 
 
 admin.site.register(Event, EventAdmin)
