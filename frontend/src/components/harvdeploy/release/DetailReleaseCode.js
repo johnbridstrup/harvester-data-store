1b6320a6961dde2d0c9baee0c916805cf51c0fd3
kkthecompguy <hnkhosamomollo3@gmail.com>
2022-11-17 19:44:13 +0300

    [detailview] schedule release deployment

--- frontend/src/components/harvdeploy/release/DetailReleaseCode.js
+++ frontend/src/components/harvdeploy/release/DetailReleaseCode.js
@@ -1,10 +1,38 @@
 import moment from "moment";
-import { useSelector } from "react-redux";
+import { useRef, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import ReactJson from "@microlink/react-json-view";
 import { JsonDiv } from "../../styled";
+import {
+  handleSelectFactory,
+  transformHarvOptions,
+} from "../../../utils/utils";
+import { queryHarvester } from "../../../features/harvester/harvesterSlice";
+import ScheduleModal from "../../modals/ScheduleModal";
+import { handleReleaseFormSubmit } from "../../../utils/services";
 
 function DetailReleaseCode(props) {
+  const [releaseObj, setReleaseObj] = useState(null);
+  const [selectedHarvId, setSelectedHarvId] = useState(null);
   const { releasecode } = useSelector((state) => state.harvdeploy);
+  const { harvesters } = useSelector((state) => state.harvester);
+  const dispatch = useDispatch();
+  const modalRef = useRef();
+  const harvOptions = transformHarvOptions(harvesters);
+
+  const handleFormSubmit = handleReleaseFormSubmit(
+    releaseObj,
+    selectedHarvId,
+    dispatch
+  );
+  const handleSelect = handleSelectFactory(setSelectedHarvId);
+
+  const modalPopUp = async (obj) => {
+    setReleaseObj((current) => obj);
+    await dispatch(queryHarvester({ fruit__name: obj.fruit?.name }));
+    modalRef.current.click();
+  };
+
   return (
     <>
       <div className="table-responsive">
@@ -14,6 +42,8 @@ function DetailReleaseCode(props) {
               <td>Version</td>
               <td>Fruit</td>
               <td>Created At</td>
+              <td>Updated At</td>
+              <td>Schedule Deployment</td>
             </tr>
           </thead>
           <tbody>
@@ -21,6 +51,24 @@ function DetailReleaseCode(props) {
               <td>{releasecode.version}</td>
               <td>{releasecode.fruit?.name}</td>
               <td>{moment(releasecode.created).format("LLLL")}</td>
+              <td>{moment(releasecode.lastModified).format("LLLL")}</td>
+              <td>
+                <span
+                  onClick={() => modalPopUp(releasecode)}
+                  className="btn btn-sm"
+                >
+                  Schedule
+                </span>
+                <button
+                  ref={modalRef}
+                  type="button"
+                  data-bs-toggle="modal"
+                  data-bs-target="#scheduleModal"
+                  style={{ display: "none" }}
+                >
+                  Schedule
+                </button>
+              </td>
             </tr>
           </tbody>
         </table>
@@ -35,6 +83,12 @@ function DetailReleaseCode(props) {
           />
         </JsonDiv>
       </div>
+      <ScheduleModal
+        handleSubmit={handleFormSubmit}
+        harvOptions={harvOptions}
+        selectedHarvId={selectedHarvId}
+        handleHarvIdSelect={handleSelect}
+      />
     </>
   );
 }
