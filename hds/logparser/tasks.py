5f901091c2f44d8b2a3917a86b85faad3ecc4e1e
John Bridstrup <john@advanced.farm>
2023-01-09 12:25:34 -0800

    [feature] Notify on success or failure of sessclip processing

--- hds/logparser/tasks.py
+++ hds/logparser/tasks.py
@@ -1,6 +1,8 @@
 import zipfile
-from celery import shared_task
+from celery import shared_task, Task
 from django.core.cache import cache
+from common.utils import build_frontend_url
+from notifications.slack import post_to_slack
 from .serializers.logfileserializers import LogFileSerializer
 from .serializers.logvideoserializers import LogVideoSerializer
 from .serializers.logsessionserializers import (
@@ -9,6 +11,46 @@ from .serializers.logsessionserializers import (
 )
 
 
+class CallbackTask(Task):
+    def _get_slack_id(self, _id):
+        creator = LogSession.objects.get(id=_id).creator
+        self.slack_id = creator.profile.slack_id
+
+    @staticmethod
+    def _create_post_message(content, slack_id=None):
+        content += f"\n<@{slack_id}>"
+        content_list = [{
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": content,
+            }
+        }]
+        message = {
+            "channel": "hds-notifications",
+            "text": "*SESSCLIP RESULTS*",
+            "blocks": content_list,
+        }
+        post_to_slack(message)
+    
+    def on_failure(self, exc, task_id, args, kwargs, einfo):
+        self._get_slack_id(args[0])
+        content = (
+            f"Failed to process sessclip.\n"
+            f"Exc: {exc.__class__.__name__}\n"
+            f"Content: {einfo}\n"
+            f"{build_frontend_url('logfiles', args[0])}"
+        )
+        self._create_post_message(content, self.slack_id)
+
+    def on_success(self, retval, task_id, args, kwargs):
+        self._get_slack_id(args[0])
+        content = (
+            f"Finished processing sessclip.\n"
+            f"{build_frontend_url('logfiles', args[0])}"
+        )
+        self._create_post_message(content, self.slack_id)
+
 @shared_task
 def async_upload_zip_file(_id):
     log_session = LogSession.objects.get(id=_id)
@@ -16,7 +58,7 @@ def async_upload_zip_file(_id):
     LogSessionSerializer.async_zip_upload(_id, zip_file)
 
 
-@shared_task
+@shared_task(base=CallbackTask)
 def perform_extraction(_id):
     log_session = LogSession.objects.get(id=_id)
     zip_file = cache.get(log_session.name)
