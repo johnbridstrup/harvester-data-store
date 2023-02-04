948dd7d04d504ee09c3a2c1eff9ad48ca6914f61
ltagliamonte <ltagliamonte@users.noreply.github.com>
2023-02-03 16:18:15 -0800

    remove module

--- terraform/module/backend/ecr.tf
+++
@@ -1,5 +0,0 @@
-module "hds-ecr-repo" {
-  source    = "git@github.com:AdvancedFarm/infrastructure.git//terraform/modules/ecr-repo?ref=master"
-  env       = var.env
-  repo_name = "hds"
-}
--- terraform/module/backend/ecs_cluster.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_ecs_cluster" "hds-cluster" {
-  name = "hds-cluster"
-
-  setting {
-    name  = "containerInsights"
-    value = "enabled"
-  }
-}
