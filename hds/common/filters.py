db1c5a214a66ef571818154a3da499a1adde7290
John Bridstrup <john@advanced.farm>
2023-02-10 09:57:32 -0800

    [refactor] HDSFilterset

--- hds/common/filters.py
+++ hds/common/filters.py
@@ -1,4 +1,7 @@
+from django_filters import rest_framework as filters
 from django_filters import Filter
+
+from common.reports import DTimeFormatter, DEFAULT_TZ
 
 
 class ListFilter(Filter):
@@ -9,3 +12,27 @@ class ListFilter(Filter):
         self.lookup_expr = "in"
         values = value.split(',')
         return super().filter(qs, values)
+
+
+class HDSFilterset(filters.FilterSet):
+    def filter_datetime(self, queryset, name, value):
+        split_dates = value.split(',')
+        if len(split_dates) != 2:
+            return queryset
+        
+        filter_dict = {}
+        tz = self.request.query_params.get("tz", DEFAULT_TZ)
+        start_str = split_dates[0]
+        end_str = split_dates[1]
+        if start_str:
+            start = DTimeFormatter.convert_to_datetime(start_str, tz)
+            filter_dict[f"{name}__gte"] = start
+        if end_str:
+            end = DTimeFormatter.convert_to_datetime(end_str, tz)
+            filter_dict[f"{name}__lte"] = end
+        return queryset.filter(**filter_dict)
+
+    def filter_bool(self, queryset, name, value):
+        # This should probably be handled more elegantly
+        value = value.lower() in ["1", "true"]
+        return queryset.filter(**{name: value})
--- hds/exceptions/filters.py
+++ hds/exceptions/filters.py
@@ -1,13 +1,13 @@
 from django_filters import rest_framework as filters
 
-from common.filters import ListFilter
+from common.filters import ListFilter, HDSFilterset
 from common.reports import DEFAULT_TZ, DTimeFormatter
 from .models import AFTException
 
 
-class AFTExceptionFilter(filters.FilterSet):
+class AFTExceptionFilter(HDSFilterset):
     # Special method filters
-    datetime_range = filters.CharFilter(method="filter_datetime") # start,end
+    datetime_range = filters.CharFilter(field_name="timestamp", method="filter_datetime") # start,end
     is_emulator = filters.CharFilter(field_name="is_emulator", method="filter_bool")
     handled = filters.CharFilter(field_name="handled", method="filter_bool")
 
@@ -34,25 +34,3 @@ class AFTExceptionFilter(filters.FilterSet):
             'report__harvester__harv_id',
             'report__harvester__location__ranch',
         )
-
-    def filter_datetime(self, queryset, name, value):
-        split_dates = value.split(',')
-        if len(split_dates) != 2:
-            return queryset
-        
-        filter_dict = {}
-        tz = self.request.query_params.get("tz", DEFAULT_TZ)
-        start_str = split_dates[0]
-        end_str = split_dates[1]
-        if start_str:
-            start = DTimeFormatter.convert_to_datetime(start_str, tz)
-            filter_dict[f"timestamp__gte"] = start
-        if end_str:
-            end = DTimeFormatter.convert_to_datetime(end_str, tz)
-            filter_dict[f"timestamp__lte"] = end
-        return queryset.filter(**filter_dict)
-
-    def filter_bool(self, queryset, name, value):
-        # This should probably be handled more elegantly
-        value = value.lower() in ["1", "true"]
-        return queryset.filter(is_emulator=value)
