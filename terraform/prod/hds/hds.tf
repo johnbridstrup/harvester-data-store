d8467e7ec7d5342e34cd8dbfd0001fd5f9fce81e
John Bridstrup <john@advanced.farm>
2023-02-13 15:56:04 -0800

    [cleanup] remove unused prom ports from prod

--- terraform/prod/hds/hds.tf
+++ terraform/prod/hds/hds.tf
@@ -7,7 +7,7 @@ locals {
   service_name             = "hds"
   service_docker_image     = "838860823423.dkr.ecr.us-west-1.amazonaws.com/hds:hds-staging-63d3896"
   healthcheck_path         = "/api/v1/healthcheck/"
-  sqs_client_metrics_ports = [9104, 9105, 9106, 9107, 9108, 9109, 9110]
+  sqs_client_metrics_ports = [9104]
   enable_prometheus_scrape = true
   service_container_memory = 4096
   service_container_cpu    = 2048
