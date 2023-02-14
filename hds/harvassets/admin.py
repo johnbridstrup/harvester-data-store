e4f1510c750f04c46ef2c881899ba6a4e47e3074
John Bridstrup <john@advanced.farm>
2023-02-14 11:21:40 -0800

    HarvAssets admin

--- hds/harvassets/admin.py
+++ hds/harvassets/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import HarvesterAsset, HarvesterAssetReport, HarvesterAssetType
 
-# Register your models here.
+
+class HarvesterAssetReportAdmin(admin.ModelAdmin):
+    list_display = ('reportTime', 'location', 'harvester', 'creator', 'created')
+    ordering = ('location', 'harvester', 'reportTime', 'creator', 'modifiedBy')
+    search_fields = ('location__ranch', 'harvester__name', 'creator__username',)
+
+
+class HarvesterAssetAdmin(admin.ModelAdmin):
+    list_display = ('harvester', 'asset', 'index', 'serial_number', 'version', 'creator', 'created', 'modifiedBy', 'lastModified')
+    ordering = ('-created',)
+
+
+class HarvesterAssetTypeAdmin(admin.ModelAdmin):
+    list_disaply = ('name', 'created', 'createdBy')
+
+
+admin.site.register(HarvesterAssetReport, HarvesterAssetReportAdmin)
+admin.site.register(HarvesterAsset, HarvesterAssetAdmin)
+admin.site.register(HarvesterAssetType, HarvesterAssetTypeAdmin)
