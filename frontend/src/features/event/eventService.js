fb63d64948df76933a81e97fae22d2bab9d52abb
kkthecompguy <hnkhosamomollo3@gmail.com>
2023-03-07 16:16:14 +0300

    [feature] picksession services and slices

--- frontend/src/features/event/eventService.js
+++ frontend/src/features/event/eventService.js
@@ -2,6 +2,7 @@ import { API_URL } from "../base/constants";
 import { axiosService } from "../base/service";
 
 export const EVENTS_URL = `${API_URL}/events/`;
+export const PICKSESSION_URL = `${API_URL}/picksessions/`;
 
 const listEvents = async (token, limit = 10) => {
   let response = await axiosService.get(`${EVENTS_URL}?limit=${limit}`, token);
@@ -32,12 +33,34 @@ const getEventTags = async (token) => {
   return response;
 };
 
+const queryPickSession = async (paramsObj, token) => {
+  const searchParams = new URLSearchParams(paramsObj);
+  const response = await axiosService.get(
+    `${PICKSESSION_URL}?${searchParams.toString()}`,
+    token
+  );
+  return response;
+};
+
+const getPickSessionById = async (id, token) => {
+  let response = await axiosService.get(`${PICKSESSION_URL}${id}/`, token);
+  return response;
+};
+
+const getPickSessionTags = async (token) => {
+  let response = await axiosService.get(`${PICKSESSION_URL}tags/`, token);
+  return response;
+};
+
 const eventService = {
   listEvents,
   getEventById,
   paginateEvent,
   queryEvent,
   getEventTags,
+  queryPickSession,
+  getPickSessionById,
+  getPickSessionTags,
 };
 
 export default eventService;
--- frontend/src/features/event/eventSlice.js
+++ frontend/src/features/event/eventSlice.js
@@ -6,6 +6,8 @@ const initialState = {
   loading: false,
   event: {},
   events: [],
+  picksession: {},
+  picksessions: [],
   tags: [],
   errorMsg: null,
   pagination: {
@@ -97,6 +99,70 @@ export const getEventTags = createAsyncThunk(
   }
 );
 
+export const queryPickSession = createAsyncThunk(
+  "event/queryPickSession",
+  async (queryObj, thunkAPI) => {
+    try {
+      const {
+        auth: { token },
+      } = thunkAPI.getState();
+      return await eventService.queryPickSession(queryObj, token);
+    } catch (error) {
+      console.log(error);
+      const message = invalidateCache(error, thunkAPI.dispatch);
+      return thunkAPI.rejectWithValue(message);
+    }
+  }
+);
+
+export const getPickSessionById = createAsyncThunk(
+  "event/getPickSessionById",
+  async (id, thunkAPI) => {
+    try {
+      const {
+        auth: { token },
+      } = thunkAPI.getState();
+      return await eventService.getPickSessionById(id, token);
+    } catch (error) {
+      console.log(error);
+      const message = invalidateCache(error, thunkAPI.dispatch);
+      return thunkAPI.rejectWithValue(message);
+    }
+  }
+);
+
+export const paginatePickSession = createAsyncThunk(
+  "event/paginatePickSession",
+  async (url, thunkAPI) => {
+    try {
+      const {
+        auth: { token },
+      } = thunkAPI.getState();
+      return await eventService.paginateEvent(url, token);
+    } catch (error) {
+      console.log(error);
+      const message = invalidateCache(error, thunkAPI.dispatch);
+      return thunkAPI.rejectWithValue(message);
+    }
+  }
+);
+
+export const getPickSessionTags = createAsyncThunk(
+  "event/getPickSessionTags",
+  async (_, thunkAPI) => {
+    try {
+      const {
+        auth: { token },
+      } = thunkAPI.getState();
+      return await eventService.getPickSessionTags(token);
+    } catch (error) {
+      console.log(error);
+      const message = invalidateCache(error, thunkAPI.dispatch);
+      return thunkAPI.rejectWithValue(message);
+    }
+  }
+);
+
 const eventSlice = createSlice({
   name: "event",
   initialState,
@@ -164,6 +230,56 @@ const eventSlice = createSlice({
         state.tags = action.payload.tags;
       })
       .addCase(getEventTags.rejected, (state, action) => {
+        state.loading = false;
+        state.errorMsg = action.payload;
+      })
+      .addCase(queryPickSession.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(queryPickSession.fulfilled, (state, action) => {
+        state.loading = false;
+        state.picksessions = action.payload.results;
+        state.pagination.count = action.payload.count;
+        state.pagination.next = action.payload.next;
+        state.pagination.previous = action.payload.previous;
+      })
+      .addCase(queryPickSession.rejected, (state, action) => {
+        state.loading = false;
+        state.errorMsg = action.payload;
+      })
+      .addCase(getPickSessionById.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(getPickSessionById.fulfilled, (state, action) => {
+        state.loading = false;
+        state.picksession = action.payload;
+      })
+      .addCase(getPickSessionById.rejected, (state, action) => {
+        state.loading = false;
+        state.errorMsg = action.payload;
+      })
+      .addCase(paginatePickSession.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(paginatePickSession.fulfilled, (state, action) => {
+        state.loading = false;
+        state.picksessions = action.payload.results;
+        state.pagination.count = action.payload.count;
+        state.pagination.next = action.payload.next;
+        state.pagination.previous = action.payload.previous;
+      })
+      .addCase(paginatePickSession.rejected, (state, action) => {
+        state.loading = false;
+        state.errorMsg = action.payload;
+      })
+      .addCase(getPickSessionTags.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(getPickSessionTags.fulfilled, (state, action) => {
+        state.loading = false;
+        state.tags = action.payload.tags;
+      })
+      .addCase(getPickSessionTags.rejected, (state, action) => {
         state.loading = false;
         state.errorMsg = action.payload;
       });
