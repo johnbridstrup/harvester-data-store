1b8fe53ec72cc00c407f3995d02d031f1fdf5582
John Bridstrup <john@advanced.farm>
2023-03-22 13:31:40 -0700

    [refactor] install docker in separate script

--- setup-venv.sh
+++ setup-venv.sh
@@ -15,31 +15,7 @@ source $VENV/bin/activate
 
 pip install -r requirements.txt
 
-echo "" && echo "Installing Docker"
-sudo apt update -y
-sudo apt install -y \
-    ca-certificates \
-    curl \
-    gnupg \
-    lsb-release \
-    prometheus
-
-if [ ! -f "/usr/share/keyrings/docker-archive-keyring.gpg" ]
-then
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | \
-        sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-    echo \
-    "deb [arch=$(dpkg --print-architecture) \
-        signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] \
-        https://download.docker.com/linux/ubuntu \
-        $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
-    sudo apt update -y
-fi
-
-sudo apt install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
-sudo groupadd docker || true
-sudo usermod -aG docker $USER
+scripts/install_docker.sh
 
 echo "" && echo "Checking for Docker Desktop install"
 
