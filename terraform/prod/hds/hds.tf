58eb017c353d872bef2abab517846c03657924aa
Paul Oliver <contact@pauloliver.dev>
2023-03-22 04:39:46 -0700

    Updates HDS docker hash to `0c09ffc`

--- terraform/prod/hds/hds.tf
+++ terraform/prod/hds/hds.tf
@@ -5,7 +5,7 @@ locals {
   frontend_url             = "https://hds.cloud.advanced.farm"
   service_port             = "8000"
   service_name             = "hds"
-  service_docker_image     = "838860823423.dkr.ecr.us-west-1.amazonaws.com/hds:hds-staging-eaf26ae"
+  service_docker_image     = "838860823423.dkr.ecr.us-west-1.amazonaws.com/hds:hds-staging-0c09ffc"
   healthcheck_path         = "/api/v1/healthcheck/"
   sqs_client_metrics_ports = [9104]
   enable_prometheus_scrape = true
