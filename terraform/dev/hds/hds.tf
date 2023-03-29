507ef082d42e94f34c8a13335ad4e63cf7ba848f
John Bridstrup <john@advanced.farm>
2023-03-29 09:59:36 -0700

    [deploy] dev containers

--- terraform/dev/hds/hds.tf
+++ terraform/dev/hds/hds.tf
@@ -5,7 +5,7 @@ locals {
   frontend_url             = "https://hds.devcloud.advanced.farm"
   service_port             = "8000"
   service_name             = "hds"
-  service_docker_image     = "082346306812.dkr.ecr.us-west-1.amazonaws.com/hds:hds-staging-1e449747"
+  service_docker_image     = "082346306812.dkr.ecr.us-west-1.amazonaws.com/hds:hds-staging-7a3f57c5"
   healthcheck_path         = "/api/v1/healthcheck/"
   sqs_client_metrics_ports = [9104]
   enable_prometheus_scrape = true
