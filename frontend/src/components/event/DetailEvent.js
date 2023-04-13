f8ad0ce68325c06320312e0300d43c14b326fedb
kkthecompguy <hnkhosamomollo3@gmail.com>
2023-04-13 21:02:18 +0300

    [refactor] use loading state for events for improved UX
    I forgot to add the loading state for events detail view
    this allows for improved user experience rather than abrupt
    update

--- frontend/src/components/event/DetailEvent.js
+++ frontend/src/components/event/DetailEvent.js
@@ -2,10 +2,11 @@ import moment from "moment";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { handleDownload } from "utils/services";
-import { darkThemeClass } from "utils/utils";
+import { LoaderDiv } from "components/styled";
+import { Loader, darkThemeClass } from "utils/utils";
 
 function DetailEvent(props) {
-  const { event } = useSelector((state) => state.event);
+  const { event, loading } = useSelector((state) => state.event);
   const { token } = useSelector((state) => state.auth);
   const { theme } = useSelector((state) => state.home);
 
@@ -17,62 +18,68 @@ function DetailEvent(props) {
 
   return (
     <>
-      <div className={`card ${cardtheme}`}>
-        <div className="card-body">
-          <div className="row mb-4">
-            <div className="col-md-4">
-              <div>
-                <strong>UUID</strong>
+      {loading ? (
+        <LoaderDiv>
+          <Loader size={50} />
+        </LoaderDiv>
+      ) : (
+        <div className={`card ${cardtheme}`}>
+          <div className="card-body">
+            <div className="row mb-4">
+              <div className="col-md-4">
+                <div>
+                  <strong>UUID</strong>
+                </div>
+                <div>{event.UUID}</div>
               </div>
-              <div>{event.UUID}</div>
+              <div className="col-md-4">
+                <div>
+                  <strong>Related Objects</strong>
+                </div>
+                <div>
+                  {event.related_objects?.map((obj, index) => (
+                    <Link to={obj.url} key={index}>
+                      <span>{obj.object}</span>
+                      <br />
+                    </Link>
+                  ))}
+                </div>
+              </div>
+              <div className="col-md-4">
+                <div>
+                  <strong>Related Files</strong>
+                </div>
+                <div>
+                  {event.related_files?.map((obj, index) => (
+                    <a
+                      href="#!"
+                      key={index}
+                      onClick={() => handleDownloadFiles(obj)}
+                    >
+                      <span>{obj.filetype}</span>
+                      <br />
+                    </a>
+                  ))}
+                </div>
+              </div>
             </div>
-            <div className="col-md-4">
-              <div>
-                <strong>Related Objects</strong>
+            <div className="row mb-4">
+              <div className="col-md-4">
+                <div>
+                  <strong>Created At</strong>
+                </div>
+                <div>{moment(event.createdAt).format("LLLL")}</div>
               </div>
-              <div>
-                {event.related_objects?.map((obj, index) => (
-                  <Link to={obj.url} key={index}>
-                    <span>{obj.object}</span>
-                    <br />
-                  </Link>
-                ))}
+              <div className="col-md-4">
+                <div>
+                  <strong>Updated At</strong>
+                </div>
+                <div>{moment(event.lastModified).format("LLLL")}</div>
               </div>
-            </div>
-            <div className="col-md-4">
-              <div>
-                <strong>Related Files</strong>
-              </div>
-              <div>
-                {event.related_files?.map((obj, index) => (
-                  <a
-                    href="#!"
-                    key={index}
-                    onClick={() => handleDownloadFiles(obj)}
-                  >
-                    <span>{obj.filetype}</span>
-                    <br />
-                  </a>
-                ))}
-              </div>
-            </div>
-          </div>
-          <div className="row mb-4">
-            <div className="col-md-4">
-              <div>
-                <strong>Created At</strong>
-              </div>
-              <div>{moment(event.createdAt).format("LLLL")}</div>
-            </div>
-            <div className="col-md-4">
-              <div>
-                <strong>Updated At</strong>
-              </div>
-              <div>{moment(event.lastModified).format("LLLL")}</div>
             </div>
           </div>
         </div>
-      </div>
+      )}
     </>
   );
 }
