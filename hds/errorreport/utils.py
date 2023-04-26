ed55d15bb611d57d399b98c95b8002807f612cee
kkthecompguy <hnkhosamomollo3@gmail.com>
2023-04-26 19:54:07 +0300

    [bugfix] updated build_list_filter for create notifications

--- hds/errorreport/utils.py
+++ hds/errorreport/utils.py
@@ -12,15 +12,58 @@ def build_list_filter(request):
         """
         listfilter = {}
 
-        # get exception codes fromrequest and filter queryset for exception code
+        # get harv_ids from query_params
+        if "harv_ids" in request.query_params:
+            harv_ids = request.query_params["harv_ids"].split(",")
+            if len(harv_ids) > 0:
+                listfilter["harvester__harv_id__in"] = harv_ids
+
+        # get locations from query_params
+        if "locations" in request.query_params:
+            locations = request.query_params["locations"].split(",")
+            if len(locations) > 0:
+                listfilter["location__ranch"] = locations
+
+        # get fruits from query_params
+        if "fruits" in request.query_params:
+            fruits = request.query_params["fruits"].split(",")
+            if len(fruits) > 0:
+                listfilter["harvester__fruit__name__in"] = fruits
+
+        # get exception codes from query_params
         if 'codes' in request.query_params:
             codes = request.query_params["codes"].split(",")
             if len(codes) > 0:
                 listfilter['exceptions__code__code__in'] = codes
 
-        # Primary exceptions flag
-        if 'exceptions__primary' in request.query_params:
-            primary = request.query_params['exceptions__primary'].lower() in ['1', 'true']
+        # get traceback from query_params
+        if 'traceback' in request.query_params:
+            traceback = request.query_params["traceback"]
+            listfilter['exceptions__traceback'] = traceback
+
+        # get traceback from query_params
+        if 'generic' in request.query_params:
+            generic = request.query_params["generic"]
+            for item in generic.split(','):
+                try:
+                    key, value = item.split('=')
+                except ValueError:
+                    pass
+                listfilter[key.strip()] = value.strip()
+
+        # get is_emulator from query params
+        if 'is_emulator' in request.query_params:
+            is_emulator = request.query_params['is_emulator'].lower() in ['1', 'true']
+            listfilter['harvester__is_emulator'] = is_emulator
+
+        # get handled from query params
+        if 'handled' in request.query_params:
+            handled = request.query_params['handled'].lower() in ['1', 'true']
+            listfilter['exceptions__handled'] = handled
+
+        # primary exceptions flag
+        if 'primary' in request.query_params:
+            primary = request.query_params['primary'].lower() in ['1', 'true']
             listfilter['exceptions__primary'] = primary
 
         return listfilter
