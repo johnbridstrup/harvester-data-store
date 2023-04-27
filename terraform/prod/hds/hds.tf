281ad56fed553b6149568c9a9ad8063733b89c97
Paul Oliver <contact@pauloliver.dev>
2023-04-27 04:22:20 -0700

    Bump HDS and Frontend dockers to `ed55d15`

--- terraform/prod/hds/hds.tf
+++ terraform/prod/hds/hds.tf
@@ -5,7 +5,7 @@ locals {
   frontend_url             = "https://hds.cloud.advanced.farm"
   service_port             = "8000"
   service_name             = "hds"
-  service_docker_image     = "838860823423.dkr.ecr.us-west-1.amazonaws.com/hds:hds-staging-5dc1def"
+  service_docker_image     = "838860823423.dkr.ecr.us-west-1.amazonaws.com/hds:hds-staging-ed55d15"
   healthcheck_path         = "/api/v1/healthcheck/"
   silk_profiling           = "false"
   silk_cprofile            = "false"
