5b8b13ba3e440c411bbe73a60677baaf4fa0a64a
John Bridstrup <john@advanced.farm>
2023-05-04 10:17:13 -0700

    [refactor] Add harvester endpoint and use common _test_get method

--- beatbox/test_utils/Endpoints.py
+++ beatbox/test_utils/Endpoints.py
@@ -7,3 +7,4 @@ class Endpoints(Enum):
     LOGIN = f"{API_VERS}/users/login/"
     ERROR = f"{API_VERS}/errorreports/"
     EXCEPTIONS = f"{API_VERS}/exceptions/"
+    HARVESTERS = f"{API_VERS}/harvesters/"
--- beatbox/tests/test_basic_get.py
+++ beatbox/tests/test_basic_get.py
@@ -3,11 +3,15 @@ from test_utils.TestBase import BaseTestCase
 
 
 class TestBasic(BaseTestCase):
+    def _test_get(self, endpoint):
+        r = self.client.get(endpoint, params=self.BASE_TEST_PARAMS)
+        self.assertOk(r)
     
     def test_get_errorreports(self):
-        r = self.client.get(Endpoints.ERROR, params=self.BASE_TEST_PARAMS)
-        self.assertOk(r)
+        self._test_get(Endpoints.ERROR)
 
     def test_get_exceptions(self):
-        r = self.client.get(Endpoints.EXCEPTIONS, params=self.BASE_TEST_PARAMS)
-        self.assertOk(r)
+        self._test_get(Endpoints.EXCEPTIONS)
+
+    def test_get_harvesters(self):
+        self._test_get(Endpoints.HARVESTERS)
