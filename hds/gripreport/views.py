deb7e6dc0138512993add4f40fe8ea90794fa98e
John Bridstrup <john@advanced.farm>
2023-05-08 12:45:09 -0700

    [bugfix] actually use the filterset class for grip reports

--- hds/gripreport/views.py
+++ hds/gripreport/views.py
@@ -1,14 +1,11 @@
 from common.viewsets import ReportModelViewSet
 
 from .models import GripReport
+from .filters import PickSessionReportFilterset
 from .serializers import GripReportSerializer
 
 
 class GripReportView(ReportModelViewSet):
     queryset = GripReport.objects.all()
     serializer_class = GripReportSerializer
-    filterset_fields = (
-        'harvester__harv_id', 
-        'event__UUID',
-        'location__ranch',
-    )
+    filterset_class = PickSessionReportFilterset
