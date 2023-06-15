97c068ae7e2dddec2466de39f00a814af92e5943
kkthecompguy <hnkhosamomollo3@gmail.com>
2023-06-15 19:26:19 +0300

    [feature] implement the emustats tags endpoint

--- hds/emulatorstats/views.py
+++ hds/emulatorstats/views.py
@@ -1,4 +1,9 @@
+from rest_framework.decorators import action
+from rest_framework.renderers import JSONRenderer
+
 from common.viewsets import ReportModelViewSet
+from common.utils import make_ok
+from hds.roles import RoleChoices
 
 from .filters import EmustatsReportFilter
 from .models import EmustatsReport
@@ -9,3 +14,22 @@ class EmuReportView(ReportModelViewSet):
     queryset = EmustatsReport.objects.all()
     serializer_class = EmustatsReportSerializer
     filterset_class = EmustatsReportFilter
+    view_permissions_update = {
+        'tags_view': {
+            RoleChoices.SUPPORT: True
+        }
+    }
+
+
+    @action(
+        methods=['GET'],
+        url_path='tags',
+        detail=False,
+        renderer_classes=[JSONRenderer]
+    )
+    def tags_view(self, request, pk=None):
+        queryset = EmustatsReport.tags.all().values_list("name", flat=True)
+        return make_ok(
+            "emulatorstats tags retrieved successfully",
+            {'tags': list(queryset)}
+        )
