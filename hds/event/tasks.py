5009227ce93e892133a0fea4bf72e27762181916
kkthecompguy <hnkhosamomollo3@gmail.com>
2023-08-24 18:42:57 +0300

    [bugfix] assert that the aux_uuids is iterable otherwise fail gracefully

--- hds/event/tasks.py
+++ hds/event/tasks.py
@@ -1,3 +1,4 @@
+from collections.abc import Iterable
 from django.apps import apps
 
 from common.celery import monitored_shared_task
@@ -10,8 +11,10 @@ def collect_aux_uuids(app, report_model, pk):
     report_inst = Report.objects.get(id=pk)
     if not hasattr(report_inst, "event"):
         return f"{report_model} does not have event field"
-        
+
     aux_uuids = report_inst.report.get("aux_uuids", [])
+    if not isinstance(aux_uuids, Iterable):
+        return f"{aux_uuids} is not iterable"
     for aux_uuid in aux_uuids:
         try:
             event = Event.objects.get(UUID=aux_uuid)
