152ce297412ab002e48eea2592f396ed38e1f35b
John Bridstrup <john@advanced.farm>
2023-09-18 10:09:21 -0700

    [refactor] Allow for _AFT_DEPLOY_TAG env var as docker image tag
    Also use sudo because the hds-deployer agent doesn't seem to be able to without it

--- make/hds.mk
+++ make/hds.mk
@@ -1,18 +1,29 @@
-.PHONY: all
+.PHONY: all clean jenkins
 all: clean login build push
+jenkins: clean login-no-profile build-jenkins push-jenkins
 
 SHA = $(shell git rev-parse --short HEAD)
 AWS_REGION = us-west-1
-NAME = hds-staging
+AWS_PROFILE = aft-prod
+_AFT_DEPLOY_TAG ?= $(SHA)
 
 login:
 	aws ecr get-login-password --region $(AWS_REGION) --profile $(AWS_PROFILE) | docker login --username AWS --password-stdin $(AWS_ACCOUNT_ID).dkr.ecr.$(AWS_REGION).amazonaws.com
 
+login-no-profile:
+	aws ecr get-login-password --region $(AWS_REGION) | sudo docker login --username AWS --password-stdin $(AWS_ACCOUNT_ID).dkr.ecr.$(AWS_REGION).amazonaws.com
+
 build:
-	docker build -t $(AWS_ACCOUNT_ID).dkr.ecr.$(AWS_REGION).amazonaws.com/hds:$(NAME)-$(SHA) ../..
+	docker build -t $(AWS_ACCOUNT_ID).dkr.ecr.$(AWS_REGION).amazonaws.com/hds:api-$(_AFT_DEPLOY_TAG) ../..
+
+build-jenkins:
+	sudo docker build -t $(AWS_ACCOUNT_ID).dkr.ecr.$(AWS_REGION).amazonaws.com/hds:api-$(_AFT_DEPLOY_TAG) ../..
 
 push:
-	docker push $(AWS_ACCOUNT_ID).dkr.ecr.$(AWS_REGION).amazonaws.com/hds:$(NAME)-$(SHA)
+	docker push $(AWS_ACCOUNT_ID).dkr.ecr.$(AWS_REGION).amazonaws.com/hds:api-$(_AFT_DEPLOY_TAG)
+
+push-jenkins:
+	sudo docker push $(AWS_ACCOUNT_ID).dkr.ecr.$(AWS_REGION).amazonaws.com/hds:api-$(_AFT_DEPLOY_TAG)
 
 clean:
 	-rm -f $(HDS_ROOT)/hds/.sqstoken; 
