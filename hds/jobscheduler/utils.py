8d5e268d5b565c25e368fd2481468668ff8dbb62
John Bridstrup <john@advanced.farm>
2023-11-15 15:11:11 -0800

    [enable] Cron and periodic jobs

--- hds/jobscheduler/utils.py
+++ hds/jobscheduler/utils.py
@@ -1,21 +1,24 @@
 from django_celery_beat.models import PeriodicTask
 
 from common.exceptions import FeatureNotEnabled
+from harvjobs.models import JobSchema
 from .models import ScheduledJob
 from .serializers import IntervalScheduleSerializer, CronTabScheduleSerializer, ClockedScheduleSerializer
 
 
 TASK_NAME = "jobscheduler.tasks.run_scheduled_job"
 
-def _get_schedule_instance(sched_def):
+def _get_schedule_instance(sched_def, allow_repeat):
     if "interval" in sched_def:
-        raise FeatureNotEnabled("Interval scheduling not enabled")
+        if not allow_repeat:
+            raise FeatureNotEnabled
         int_sched_ser = IntervalScheduleSerializer(data=sched_def['interval'])
         int_sched_ser.is_valid()
         inst = int_sched_ser.save()
         return {"interval": inst}
     elif "crontab" in sched_def:
-        raise FeatureNotEnabled("Crontab scheduling not enabled")
+        if not allow_repeat:
+            raise FeatureNotEnabled
         cron_sched_ser = CronTabScheduleSerializer(data=sched_def['crontab'])
         cron_sched_ser.is_valid()
         inst = cron_sched_ser.save()
@@ -30,7 +33,11 @@ def _get_schedule_instance(sched_def):
 def create_periodic_task(sched_job_id):
     sched_job = ScheduledJob.objects.get(id=sched_job_id)
     job_def = sched_job.job_def
-    sched = _get_schedule_instance(job_def["schedule"])
+    name = job_def.get("jobtype")
+    vers = job_def.get("schema_version")
+    schema = JobSchema.objects.get(jobtype__name=name, version=vers)
+    
+    sched = _get_schedule_instance(job_def["schedule"], allow_repeat=schema.allows_repeats)
     task_fields = {
         "task": TASK_NAME,
         "name": f"scheduled_job_{sched_job_id}_{job_def['jobtype']}",
