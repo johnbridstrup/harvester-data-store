d35f91f662c95d2203e60f0fcb3864096c8d34fc
kkthecompguy <hnkhosamomollo3@gmail.com>
2023-12-09 01:53:36 +0300

    [refactor] implement s3file model methods

--- hds/s3file/models.py
+++ hds/s3file/models.py
@@ -1,6 +1,10 @@
-from django.db import models
+import os
+import shutil
 from django.urls import reverse
 from urllib.parse import urljoin
+from django.db import models
+from django.conf import settings
+from django.core.files.storage import default_storage
 
 from common.models import CommonInfo
 from common.utils import media_upload_path
@@ -26,6 +30,24 @@ class S3File(EventModelMixin, CommonInfo):
     def delete_from_s3(self):
         self.file.delete() # deletes file from storages
         self.save()
+
+    @property
+    def download_dir(self):
+        return os.path.join(settings.DOWNLOAD_DIR, f"{self.pk}")
+
+    @property
+    def download_path(self):
+        return os.path.join(self.download_dir, os.path.basename(self.file.name))
+
+    def download(self):
+        os.makedirs(self.download_dir, exist_ok=True)
+        with default_storage.open(self.file.name, "rb") as file:
+            with open(self.download_path, "wb") as dest:
+                dest.write(file.read())
+
+    def clean_download(self):
+        if os.path.isdir(self.download_dir):
+            shutil.rmtree(self.download_dir)
 
 
 class S3FileMixin(models.Model):
