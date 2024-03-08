e69f810884eea687694ab1d73d4b9a5752fc4949
John Bridstrup <john@advanced.farm>
2024-03-07 16:23:09 -0800

    [testing] assert grip extraction works as expected
    importantly that the grip is linked to a cand

--- hds/gripreport/tests.py
+++ hds/gripreport/tests.py
@@ -44,3 +44,32 @@ class GripReportTestCase(HDSAPITestBase):
         self.assertEqual(cand.score, exp_cand["score"])
         self.assertDictEqual(cand.candidate_data, exp_cand)
 
+    def test_grip_extraction(self):
+        self.post_picksess_report()
+        num_exp_grips = len(self.picksess_data["data"]["grip"])
+
+        self.assertEqual(Grip.objects.count(), num_exp_grips)
+
+        # make sure grip info extracted correctly
+        exp_grip = self.picksess_data["data"]["grip"][0]
+        grip = Grip.objects.get(grip_start_ts=exp_grip["grip_start_ts"])
+
+        self.assertEqual(grip.grip_end_ts, exp_grip["grip_end_ts"])
+        self.assertEqual(grip.grip_start_ts, exp_grip["grip_start_ts"])
+        self.assertEqual(grip.pick_result, exp_grip["pick_result"])
+        self.assertEqual(grip.grip_result, exp_grip["grip_result"])
+        self.assertEqual(grip.robot_id, exp_grip["robot_id"])
+        self.assertDictEqual(grip.grip_data, exp_grip)
+        self.assertTrue(grip.success)
+        self.assertEqual(grip.grip_duration, exp_grip["grip_end_ts"] - exp_grip["grip_start_ts"])
+
+        # make sure the right cand was linked
+        cand_list = self.picksess_data["data"]["cand"]
+        exp_cand = None
+        for cand in cand_list:
+            if cand["cand_id"] == exp_grip["cand_id"] and cand["robot_id"] == exp_grip["robot_id"]:
+                exp_cand = cand
+                break
+        
+        cand_obj = Candidate.objects.get(cand_id=exp_cand["cand_id"], robot_id=exp_cand["robot_id"])
+        self.assertEqual(grip.candidate, cand_obj)
