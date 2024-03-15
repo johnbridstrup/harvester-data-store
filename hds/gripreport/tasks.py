d7ba9a66cef3a4727e22eded617f5d1b22caf2c6
John Bridstrup <john@advanced.farm>
2024-03-15 15:49:38 -0700

    [feature] Grip report on demand extraction task

--- hds/gripreport/tasks.py
+++ hds/gripreport/tasks.py
@@ -1,6 +1,7 @@
 import structlog
 
 from django.contrib.auth.models import User
+from django.core.paginator import Paginator
 from common.celery import monitored_shared_task
 from common.fileloader import get_client
 
@@ -25,6 +26,19 @@ def download_gripreport(event, creator):
     return rep.id
 
 @monitored_shared_task
-def extract_grip_report(report_id):
+def extract_grip_report(report_id, log=True):
     GripReportSerializer.extract_report(report_id)
-    logger.info(f"Grip report {report_id} extracted")
+    if log:
+        logger.info(f"Grip report {report_id} extracted")
+
+@monitored_shared_task
+def extract_grip_reports(per_page=500, **filters):
+    qs = GripReport.objects.filter(**filters) 
+    paginator = Paginator(qs, per_page)
+    logger.info(f"Extracting {paginator.count} grip reports")
+    for page in range(1, paginator.num_pages + 1):
+        logger.info(f"Extracting page {page} of {paginator.num_pages}")
+        for report in paginator.page(page).object_list:
+            extract_grip_report(report.id, log=False)
+        logger.info(f"Page {page} extracted")
+        
